b86b5ba6d599ea5aef026a6b998c2341c7a6ca74
januszpolak <janusz.polak@interia.pl>
2019-06-17 19:14:31 +0100

    change rate

---
+++ overtime.py
@@ -0,0 +1,14 @@
+hours = float(input('How many hours did you work? '))
+rate = 8.21
+over = 1.5* rate
+income = hours*rate
+diference = hours-40
+nhs = income*0.01
+tax = income*0.12
+alltaxes = nhs+tax
+if hours <41:
+    print("You earned £", income-alltaxes)
+else:
+    print("You earned £", income+diference-alltaxes)
+print("You should work harder and longer to be rich ;PP")
+input("Aby zakończyć program naciśnij ENTER")
