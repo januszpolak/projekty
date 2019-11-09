e8b3c37d5e87e295b4cb3cf01eef06c030b483fd
januszpolak <janusz.polak@interia.pl>
2019-11-09 11:18:12 +0000

    alarm with sound in python

---
+++ alarm.py
@@ -0,0 +1,14 @@
+import datetime
+import pyglet
+alarmHour = int(input("What time do you want to wake up?"))
+alarmMinute = int(input("What minut do you want t wake up?"))
+
+while True:
+    if(alarmHour == datetime.datetime.now().hour and
+       alarmMinute == datetime.datetime.now().minute):
+        print("wake up")
+        music = pyglet.resource.media('bomb.wav', streaming=False)
+        music.play()
+        pyglet.app.run()
+        
+        break
