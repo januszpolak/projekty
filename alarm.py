96514f6fb748ad4507a2d9d51f4bb01047d79cd2
januszpolak <janusz.polak@interia.pl>
2019-11-09 11:52:07 +0000

    play sound

--- alarm.py
+++ alarm.py
@@ -7,6 +7,7 @@ while True:
     if(alarmHour == datetime.datetime.now().hour and
        alarmMinute == datetime.datetime.now().minute):
         print("wake up")
+	print("to cancel press CTRL+C")
         music = pyglet.resource.media('bomb.wav', streaming=False)
         music.play()
         pyglet.app.run()
