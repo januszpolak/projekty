c0969054839f9b8cd81c25af29f0cd6cef1659ff
januszpolak <janusz.polak@interia.pl>
2020-02-16 20:59:07 +0100

    new small apps

--- nasa/nasa.py
+++ nasa/nasa.py
@@ -2,6 +2,8 @@ import requests
 import os
 import webbrowser
 from googletrans import Translator
+from urllib.request import urlretrieve
+
 
 url = 'https://api.nasa.gov/planetary/apod?api_key=D5eW7d8pzrELRM0UxNJl8diqhHYVkc98YoMalk6M'
 
@@ -9,8 +11,10 @@ res = requests.get(url)
 data = res.json()
 picture = data['url']
 
-#open picture in browser
+#open picture in browser and save it to local directory
 webbrowser.open(picture)
+filename = picture.strip().split('/')[-1]
+urlretrieve(picture.strip(), filename)
 
 #os.system("gsettings set org.gnome.desktop.background picture-uri " + picture)
 
