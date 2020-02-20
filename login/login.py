70a28d22a5d2a425598a80234d6d10a3e6b9c1b5
januszpolak <janusz.polak@interia.pl>
2020-02-20 13:17:43 +0100

    update login app -- keep login and password in separate file

--- login/login.py
+++ login/login.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
 
 from tkinter import *
+from configparser import ConfigParser
 from playsound import playsound
 
 
 root = Tk()
 root.geometry('400x250')
-root.title('Login to HELL :))')
+root.title('Login Panel')
 root.configure(background='#a33540')
 
+# storage login and password in different config file -- config.ini
+config = ConfigParser()
+config.read('config.ini')
+login = config['DEFAULT']['login']
+password = config['DEFAULT']['password']
 
-login = 'janusz'
-password = 'dawajneta'
-ble1 = 'Wchodzisz do piekła :))'
-ble2 = 'Znajdz prawdziwy login i hasło, aby wejść ... '
+get_in = 'Brawo !!! Udało się, wchodzisz :))'
+get_out = 'Podaj prawidłowy login i hasło, aby wejść ... '
 
 def update():
     label1.delete('1.0', END)
@@ -22,13 +26,14 @@ def update():
 def check():
     if login_input.get() == login and pass_input.get() == password:
         update()
-        label1.insert(INSERT, ble1)
+        label1.insert(INSERT, get_in)
         playsound('appl.mp3')
         
     else:
         update()
-        label1.insert(INSERT, ble2)
+        label1.insert(INSERT, get_out)
         playsound('evil.wav')
+
 
 label = Label(root, text='Wpisz login i hasło ...', bg='#a33540', font=30, pady=25)
 label.pack()
@@ -39,7 +44,7 @@ login_input.pack()
 pass_input = Entry(root, width=20, bd=3, show='*')
 pass_input.pack()
 
-btn = Button(root, text='Submit', command=check)
+btn = Button(root, text='Potwierdź', command=check)
 btn.pack()
 
 label1 = Text(root, bg='white')
